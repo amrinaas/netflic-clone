e3b6b8c4acdb47cd24f28c3a111a78b0647a1100
amrinaas <amrinaas96@gmail.com>
2024-06-18 14:50:39 +0700

    Fix styling

--- app/browse/@popular/page.js
+++ app/browse/@popular/page.js
@@ -6,9 +6,8 @@ export default async function BrowsePage() {
   const movie = result.results;
 
   return (
-    <section className="ml-10">
-      <h1 className="font-semibold text-xl mb-2">Popular Now</h1>
-      <Slider movies={movie} />
+    <section className="">
+      <Slider movies={movie} title={'Popular Now'} />
     </section>
   );
 }
--- components/movie/slider-item.js
+++ components/movie/slider-item.js
@@ -1,19 +1,23 @@
 import Link from 'next/link';
 
-export default function SliderItem({ poster_path, title, width, id }) {
+export default function SliderItem({
+  poster_path,
+  backdrop_path,
+  title,
+  width,
+  id,
+  isGenre,
+}) {
   return (
-    // <Link
-    //   style={{ width: `${width}%` }}
-    //   className="text-white relative ease-in focus-within:-translate-x-1/4 duration-500 focus:translate-x-1/4 hover:translate-x-1/4 focus:scale-150 focus:z-10 hover:scale-150 hover:z-10 py-0 px-1 inline-block first-of-type:pl-0 last-of-type:pr-0"
-    //   href={`/movie/${id}`}
-    // >
     <Link
       style={{ width: `${width}%` }}
-      className="py-0 px-1 inline-block first-of-type:pl-0 last-of-type:pr-0"
+      className="py-0 px-1 inline-block bg-contain hover:scale-150 ease-in duration-500 transition-all"
       href={`/popular/${id}`}
     >
       <img
-        src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+        src={`https://image.tmdb.org/t/p/original/${
+          !isGenre ? poster_path : backdrop_path
+        }`}
         alt={title}
         className="w-full max-w-full block rounded"
       />
