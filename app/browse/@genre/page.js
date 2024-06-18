3b5bc1771907677927e99644e0cfa98f77c491ee
amrinaas <amrinaas96@gmail.com>
2024-06-18 20:51:55 +0700

    Show only 5 genre in homepage

--- app/browse/@genre/page.js
+++ app/browse/@genre/page.js
@@ -5,9 +5,15 @@ export default async function MovieByGenre() {
   const response = await getListOfGenre();
   const genres = response.genres;
 
+  const getMultipleRandom = (arr, num) => {
+    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+    return shuffled.slice(0, num);
+  };
+
   return (
     <section>
-      {genres.map(async (genre, i) => {
+      {getMultipleRandom(genres, 5).map(async (genre, i) => {
         const result = await getMovieByGenre(genre.id);
         const movie = result.results;
         return (
